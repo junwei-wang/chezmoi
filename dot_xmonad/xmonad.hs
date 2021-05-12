f91a2532bbb2ffe6af1b3bc5738d7e27c0137db8
Junwei Wang <junwei.wang@cryptoexperts.com>
2021-05-12 11:09:51 +0200

    Update xmonad

---
+++ dot_xmonad/xmonad.hs
@@ -0,0 +1,91 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import qualified XMonad.StackSet as W
+
+import Data.Maybe (fromJust)
+import qualified Data.Map as M
+
+import XMonad.Util.Dmenu
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.SpawnOnce
+
+myTerminal :: String
+myTerminal = "$HOME/.cargo/bin/alacritty"
+
+myBorderWidth :: Dimension
+myBorderWidth = 3           -- Sets border width for windows
+
+myNormColor :: String
+myNormColor   = "#9d91bb"   -- Border color of normal windows
+
+myFocusColor :: String
+myFocusColor  = "#46d9ff"   -- Border color of focused windows
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "volumeicon &" 
+    spawnOnce "nm-applet &" 
+    spawnOnce "conky -c $HOME/.config/conky/xmonad.conkyrc"
+    spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
+
+
+
+-- myWorkspaces = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
+myWorkspaces = [" dev ", " www ", " sys ", " doc ", " vbox ", " chat ", " mus ", " vid ", " gfx "]
+myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
+
+clickable ws = "<action=xdotool key super+"++show i++">"++ws++"</action>"
+    where i = fromJust $ M.lookup ws myWorkspaceIndices
+
+-- myKeys :: [(String, X ())]
+myKeys =
+    -- Xmonad
+        [ ("M-C-r", spawn "xmonad --recompile")  -- Recompiles xmonad
+        , ("M-S-r", spawn "xmonad --restart")    -- Restarts xmonad
+
+    -- Run Prompt
+        -- , ("M-p", spawn "dmenu run -i -p \"Run: \"") -- Dmenu
+        , ("M-S-p", spawn "rofi --show drun")    -- Dmenu
+        ]
+
+main = do
+  xmproc <- spawnPipe "xmobar -X 0 $HOME/.config/xmobar/xmobarrc"
+  xmonad $ docks defaultConfig
+    { manageHook         = manageDocks <+> manageHook defaultConfig
+    ,layoutHook = avoidStruts $ layoutHook defaultConfig
+     -- this must be in this order, docksEventHook must be last
+    , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
+    , startupHook        = myStartupHook
+    , terminal = myTerminal
+    , workspaces = myWorkspaces
+    , borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormColor
+    , focusedBorderColor = myFocusColor
+    , logHook = dynamicLogWithPP $ namedScratchpadFilterOutWorkspacePP $ xmobarPP
+              -- the following variables beginning with 'pp' are settings for xmobar.
+              { ppOutput = \x -> hPutStrLn xmproc x                          -- xmobar on monitor 1
+                              -- >> hPutStrLn xmproc1 x                          -- xmobar on monitor 2
+                              -- >> hPutStrLn xmproc2 x                          -- xmobar on monitor 3
+              , ppCurrent = xmobarColor "#98be65" "" . wrap "[" "]"           -- Current workspace
+              , ppVisible = xmobarColor "#98be65" "" . clickable              -- Visible but not current workspace
+              , ppHidden = xmobarColor "#82AAFF" "" . wrap "*" "" . clickable -- Hidden workspaces
+              , ppHiddenNoWindows = xmobarColor "#c792ea" ""  . clickable     -- Hidden workspaces (no windows)
+              , ppTitle = xmobarColor "#b3afc2" "" . shorten 60               -- Title of active window
+              , ppSep =  "<fc=#666666> <fn=1>|</fn> </fc>"                    -- Separator character
+              , ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"            -- Urgent workspace
+              , ppExtras  = [windowCount]                                     -- # of windows current workspace
+              , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]                    -- order of things in xmobar
+              }
+    -- , modMask = mod4Mask       -- Rebind Mod to the Windows key
+    } `additionalKeysP` myKeys
+    -- [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    -- , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- , ((0, xK_Print), spawn "scrot")
+    -- ]
---
+++ private_dot_config/private_fish/conf.d/00_path.fish
@@ -0,0 +1,2 @@
+set -gx PATH $HOME/bin $HOME/.emacs.d/bin $HOME/.local/bin $PATH
+set -gx PATH $HOME/.cargo/bin $PATH
