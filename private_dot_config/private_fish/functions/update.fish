80b251e4c0e67134395fb05848c37a9b9d59520a
Junwei Wang <me@jwa.ng>
2021-09-26 11:00:25 +0200

    Sync from mbp

--- private_dot_config/private_fish/functions/update.fish
+++ private_dot_config/private_fish/functions/update.fish
@@ -1,30 +1,30 @@
 function update -d "System update"
     if type -q brew
-        echo "Updating Homebrew"
+        echo ">>> Updating Homebrew"
         brew update
         brew upgrade
+        echo ">>> Clean Homebrew"
         brew cleanup
     else if type -q pacman
-        echo "Update pacman"
+        echo ">>> Update pacman"
         sudo pacman -Syu
-        echo "Clean pacman"
+        echo ">>> Clean pacman"
         sudo pacman -R (pacman -Qtdq)
     end
 
     echo ""
 
     if type -q doom
-        echo "Updating doom"
+        echo ">>> Updating doom"
         doom upgrade
-        doom purge
     end
 
     if test -d ~/.cb/cryptobib
-        echo "Pulling latest cryptobib"
+        echo ">>> Pulling latest cryptobib"
         cd ~/.cb/cryptobib
         git pull
     else
-        echo "Cloning cryptobib"
+        echo ">>> Cloning cryptobib"
         mkdir -p ~/.cb
         git clone https://github.com/cryptobib/export ~/.cb/cryptobib
     end
@@ -32,19 +32,26 @@ function update -d "System update"
 
 
     if test -d ~/github/org
-        echo "Pulling latest org repository from GitHub"
+        echo ">>> Pulling latest org repository from GitHub"
         cd ~/github/org
         git pull
         if git diff-index --quiet HEAD
             echo Clean
         else
-            echo "WARNING: Need to commit org"
+            echo ">>> WARNING: Need to commit org"
         end
     else
-        echo "Cloning github/junwei-wang:org"
+        echo ">>> Cloning github/junwei-wang:org"
         mkdir -p ~/github
         git clone git@github.com:junwei-wang/org.git ~/github/org
     end
     echo ""
 
+    switch (uname)
+        case Darwin
+            # update simple-bar
+            echo ">>> Update simple-bar"
+            cd $HOME/Library/Application\ Support/Übersicht/widgets/simple-bar
+            git pull
+    end
 end
