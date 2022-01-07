2e7cf0eefc3698c22b04187c3f1945d424938052
Junwei Wang <me@jwa.ng>
2022-01-07 09:55:05 +0100

    Update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,3 +2,6 @@ set -gx LC_ALL en_US.UTF-8
 set -gx LC_CTYPE en_US.UTF-8
 fish_add_path /usr/local/sbin
 fish_add_path /usr/local/sbin
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
